896ce7069ae3623b39d6405d230c2fd40236de3c
SolisTenebrum <solistenebrum@icloud.com>
2024-11-24 19:29:00 +0500

    added MainContent component with main tag

---
+++ src/components/MainContent.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HomeSlider from './HomeSlider.vue'
+import ProductList from './ProductList.vue'
+</script>
+
+<template>
+  <main>
+    <home-slider></home-slider>
+    <product-list></product-list>
+  </main>
+</template>
+
+<style scoped></style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,15 +1,13 @@
 <script setup>
 import AppHeader from '@/components/AppHeader.vue'
-import HomeSlider from '@/components/HomeSlider.vue'
-import ProductList from '@/components/ProductList.vue'
 import AppFooter from '@/components/AppFooter.vue'
 import CartModal from '@/components/CartModal.vue'
+import MainContent from '@/components/MainContent.vue'
 </script>
 
 <template>
   <app-header></app-header>
-  <home-slider></home-slider>
-  <product-list></product-list>
+  <main-content></main-content>
   <app-footer></app-footer>
   <cart-modal></cart-modal>
 </template>
